45c852f9dce3bc77a3e28ff7ff22809fa1c4b98c
critter42 <critter42@gmail.com>
2018-11-17 00:36:41 -0600

    Testing Deployment

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>TAYFIRE</title>
+  <meta name="description" content="TAYFIRE - The Blazing Social Media Network!">
+  <meta name="author" content="Team TayFire!">
+
+  <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+</head>
+
+<body>
+  <img src ="images/TayFire.gif">
+</body>
+</html>
