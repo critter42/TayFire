087146075d1fde78cae4ccfb6bfa683e52e96fd2
critter42 <critter42@gmail.com>
2018-11-17 21:38:12 -0600

    Update tayfireconfig.php

--- include/tayfireconfig.php
+++ include/tayfireconfig.php
@@ -1,19 +1,19 @@
 <?PHP
 require_once("./include/tayfiremembers.php");
 
-$tayfiremembers = new TayfireUsersite();
+$TayFireUsersite = new TayfireUsersite();
 
 //Provide your site name here
-$tayfiremembers->SetWebsiteName('tayfire.com');
+$TayFireUsersite->SetWebsiteName('tayfire.com');
 
 //Provide the email address where you want to get notifications
-$tayfiremembers->SetAdminEmail('dchmbrln@memphis.edu');
+$TayFireUsersite->SetAdminEmail('dchmbrln@memphis.edu');
 
 //Provide your database login details here:
 //hostname, user name, password, database name and table name
 //note that the script will create the table (for example, fgusers in this case)
 //by itself on submitting register.php for the first time
-$tayfiremembers->InitDB(/*hostname*/'localhost',
+$TayFireUsersite->InitDB(/*hostname*/'localhost',
                       /*username*/'TayFire',
                       /*password*/'T4yF1r3!',
                       /*database name*/'TayFireDev',
@@ -21,6 +21,6 @@ $tayfiremembers->InitDB(/*hostname*/'localhost',
 
 //For better security. Get a random string from this link: http://tinyurl.com/randstr
 // and put it here
-$tayfiremembers->SetRandomKey('QAAucYfpdlHyTGA');
+$TayFireUsersite->SetRandomKey('QAAucYfpdlHyTGA');
 
 ?>
