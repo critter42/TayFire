d16b4e8b9b61370f2eacd61d3005b012f3196663
Xia Fang <arcseneon@gmail.com>
2018-11-29 14:08:14 -0600

    created profile page

---
+++ profile.php
@@ -0,0 +1,29 @@
+<?PHP
+require_once("./include/tayfireconfig.php");
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
+  <head>
+    <title>TayFire Profile page</title>
+  </head>
+  <body>
+  	<div>
+    <p>Your Profile</p>
+    </div>
+    <div>
+    <p>Show full name
+    <br>Show e-mail
+    <br>Show number of followers</p>
+    </div>
+    <div>
+    <p>List followed users
+    <br>List feeds</p>
+    </div>
+    <div>
+    <p>Show number of posts
+    <br>Show 5 most recent posts</p>
+    </div>
+    <div class='container'><a href="change-email.php">Change E-Mail</a></div>
+    <div class='container'><a href="change-pwd.php">Change Password</a></div>
+  </body>
+</html>
