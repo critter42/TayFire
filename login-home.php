1bd804bb6b94ccd5659aec2e96fb000d2506b069
critter42 <critter42@gmail.com>
2018-11-30 13:52:06 -0600

    Update login-home.php

--- login-home.php
+++ login-home.php
@@ -39,8 +39,8 @@ $posts = $TayFireUsersite->GetUserPosts($userid);
 if($posts->num_rows > 0) {
 	while($row = $posts->fetch_assoc()) {
 	    
-	   echo $row["p_title"]." <br>" . $row["p_content"]." <br>";
-	   echo "Comments";
+	   echo $row["p_title"]." <br />" . $row["p_content"]." <br />";
+	   echo "Comments <br />";
 	  
 	 }
 }
