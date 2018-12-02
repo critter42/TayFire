ee1d9f2fc2111e2e93a8ecf96815daedaf885069
critter42 <critter42@gmail.com>
2018-12-01 20:10:38 -0600

    Update login-home.php

--- login-home.php
+++ login-home.php
@@ -38,13 +38,13 @@ if($posts->num_rows > 0) {
 	   echo "<a href='post.php?postid=".$row["post_id"]."'>".$row["p_title"]." <br /></a>" . $row["p_content"]." <br /><br />";
 	   echo "Comments <br />";
 	   $comments = $TayFireUsersite->GetComments($row["post_id"]);
-
+	   echo "<center><img src=\"../bootstrap/img/rainbow.gif\"></center>";
 	 }
 }
 else {
     echo "No posts! <br />";
 }
-echo "<center><img src=\"../bootstrap/img/rainbow.gif\"></center>";
+
 ?>	
 <p><a href='logout.php'>Logout</a></p>
 <p><a href='change-pwd.php'>Change password</a></p>
