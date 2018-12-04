9948cb3820d2d649e12b9d0b2197338a91e4a52f
critter42 <critter42@gmail.com>
2018-12-04 02:26:50 -0600

    Update profile.php

--- profile.php
+++ profile.php
@@ -23,7 +23,36 @@ if(!$TayFireUsersite->CheckLogin())
 	    <p>Your Profile</p>
       </div>
       <div>
+
 <?php
+
+if (empty($_GET['profileid'])){
+	$isFollowed = $_SESSION['user_id_of_user'];
+}
+else
+{
+	$isFollowed = $_GET['profileid'];
+}
+$user = $_SESSION['user_id_of_user'];
+if(isset($_POST['Follow']))
+{
+	$conn  = mysqli_connect("localhost","TayFire","T4yF1r3!","TayFire");
+	$qry2 = "Select isFollowed_id,follower_id FROM Follow WHERE isFollowed_id ='".$isFollowed."' AND follower_id = '".$user."'";
+	$result = mysqli_query($conn,$qry2);
+	
+	if (!$result || mysqli_num_rows($result)==0)
+	{
+		$qry = "Insert Into PostLikes (post_id,liker_id) VALUES('".$isFollowed."','".$user."')";
+		$result2 = mysqli_query($conn,$qry);
+		echo "<meta http-equiv='refresh' content='0'>";
+	}
+	else
+	{
+		$del = "DELETE FROM PostLikes WHERE post_id ='".$isFollowed."' AND liker_id = '".$user."'";
+		$result3 = mysqli_query($conn,$del);
+		echo "<meta http-equiv='refresh' content='0'>";
+	}
+}
 if (empty($_GET['profileid'])){
 	$profile_id = $_SESSION['user_id_of_user'];
 }
@@ -34,7 +63,13 @@ else
         echo "<p>Name: ".$TayFireUsersite->GetNamefromID($profile_id);
         echo "<br>Email: ".$TayFireUsersite->GetUserEmailfromID($profile_id);
         echo "<br># Followers: ".$TayFireUsersite->GetFollowerCount($profile_id)."</p>";
+		echo "<form id='newFollow' action='profile.php?profileid=".$profile_id."' method='post' accept-charset='UTF-8'><br />";
+	   echo "    <div class='container'>";
+	   echo " 		<input type='submit' name='Follow' value='Follow' />";
+       echo "		</div>";
+	   echo "     </form>";
 ?>
+
       </div>
       <div>
         <p>People I follow:<br />
